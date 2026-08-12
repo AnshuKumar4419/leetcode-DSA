229d9e8a62c8532211406753866ecacf7246519e
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-08-13 00:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 69.8 MB (35.09%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,7 +6,7 @@ public:
         int l = 0, r = 0, maxlen = 0;
         while(r < n) {
             if(nums[r] == 0) zeros++;
-            if(zeros > k) {
+            while(zeros > k) {
                 if(nums[l] == 0) zeros--;
                 l++;
             }
