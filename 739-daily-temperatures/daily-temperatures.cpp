df1bf80f7b3120d0682f766f5efe3aa894d10be8
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-09-10 23:31:05 +0530

    Time: 16 ms (80.80%) | Memory: 102.9 MB (78.95%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -6,11 +6,6 @@ public:
         vector<int> nums(n);
 
         for(int i = n - 1; i >= 0; i--) {
-            if(st.empty()) {
-                st.push(i);
-                nums[i] = 0;
-                continue;
-            }
             while(!st.empty() && temp[i] >= temp[st.top()]) {
                 st.pop();
             }
