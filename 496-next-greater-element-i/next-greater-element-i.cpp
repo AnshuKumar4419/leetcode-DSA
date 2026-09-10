66daea1b4eb1446eab3b025321aaf8902f5c5ab8
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-09-10 08:02:53 +0530

    Time: 1 ms (54.06%) | Memory: 12.8 MB (62.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,22 +1,34 @@
-class Solution {
+class Solution 
+{
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> ans;
-        for(int i = 0; i < n; i++) {
-            int Greater = -1;
-            int found = 0;
-            for(int j = 0; j < m; j++) {
-                if(nums1[i] == nums2[j]) {
-                    found = 1;
-                }
-                if(nums1[i] < nums2[j] && found == 1) {
-                    Greater = nums2[j];
-                    break;
-                }
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) 
+    {
+        unordered_map<int, int> m;
+        stack<int> s;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) 
+        {
+            while (s.size() > 0 && s.top() <= nums2[i]) 
+            {
+                s.pop();
             }
-            ans.push_back(Greater);
+
+            if (s.empty()) 
+            {
+                m[nums2[i]] = -1;
+            }
+            else 
+            {
+                m[nums2[i]] = s.top();
+            }
+
+            s.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) 
+        {
+            ans.push_back(m[nums1[i]]);
         }
         return ans;
     }
