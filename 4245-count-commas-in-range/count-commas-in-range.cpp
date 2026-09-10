c050a511fcaab015582ec75a3ab24e801993de1a
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-09-10 15:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int count = 0;
-        for(int i = 1000; i <= n; i++) {
-            count++;
-        }
-        return count;
+        if(n >= 1000) return n - 1000 + 1;
+        return 0;
     }
 };
