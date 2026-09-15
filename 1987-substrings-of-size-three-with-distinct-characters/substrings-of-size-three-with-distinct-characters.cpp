b0a49e23ff80555ae0c538a72d683ae603d6492e
AnshuKumar4419 <anshukumarocket@gmail.com>
2026-09-15 14:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.49%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -4,11 +4,7 @@ public:
         int n = s.size();
         int ans = 0;
         for(int i = 0; i < n - 2; i++) {
-            unordered_set<char> st;
-            st.insert(s[i]);
-            st.insert(s[i + 1]);
-            st.insert(s[i + 2]);
-            if(st.size() == 3) ans++;
+            if((s[i] != s[i + 1]) && (s[i] != s[i + 2]) && (s[i + 1] != s[i + 2])) ans++;
         }
         return ans;
     }
